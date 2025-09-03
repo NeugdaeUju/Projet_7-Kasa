ebf4a48620189697280471f1731b342485250579
Leïla Pelletier <leila.plltr@gmail.com>
2025-09-03 13:57:58 +0200

    Ajout du logo au header

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,10 +1,12 @@
 // Création des routes de l'application
 
 import { NavLink, Outlet } from "react-router-dom";
+import logo from './assets/LOGO.svg'
 
 function Root () {
     return <>
     <header>
+        <img src={logo} alt='Logo Kasa'></img>
         <nav>
             <NavLink to='/'>Accueil</NavLink>
             <NavLink to='/a-propos'>A Propos</NavLink>
