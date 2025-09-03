79b080448936217d39422cfb86639f73f3fe6575
Leïla Pelletier <leila.plltr@gmail.com>
2025-09-03 10:01:34 +0200

    Ajout de la page d'erreur au code root

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import Root from './Root.jsx'
+import PageError from './PageError.jsx'
 
 const router = createBrowserRouter ([
   {
     path : '/',
     element : <Root/>,
+    errorElement : <PageError/>,
     children : [{
       path : 'a-propos',
       element : <div>Page A Propos</div>
