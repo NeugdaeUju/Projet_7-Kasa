261c28994a46417566c59716fcd4ea276f14d343
NeugdaeUju <leila.plltr@gmail.com>
2025-09-04 12:07:40 +0200

    Création du composant pour le le bandeau, création de la page d'accueil, modification des routes pour permettre l'affichage correct de la page d'accueil

---
+++ src/components/pages/Page_Accueils/Accueil.jsx
@@ -0,0 +1,11 @@
+import Footer from '../../components_allPages/Footer.jsx'
+import Hero from './Hero.jsx'
+
+function Accueil () {
+    return <>
+        <Hero />
+        <Footer />
+    </>
+}
+
+export default Accueil
