4b92f3258ad1d79ffed4a383516862c26eaf56ee
NeugdaeUju <leila.plltr@gmail.com>
2025-09-04 12:32:38 +0200

    Création de la div qui contiendra tous les logements + correction des styles (marges)

--- src/components/pages/Page_Accueils/Accueil.jsx
+++ src/components/pages/Page_Accueils/Accueil.jsx
@@ -1,9 +1,11 @@
 import Footer from '../../components_allPages/Footer.jsx'
 import Hero from './Hero.jsx'
+import Logement from './AllLogements.jsx'
 
 function Accueil () {
     return <>
         <Hero />
+        <Logement/>
         <Footer />
     </>
 }
---
+++ src/components/pages/Page_Accueils/AllLogements.jsx
@@ -0,0 +1,11 @@
+import '../../../styles/style_accueil_logement.css'
+
+function Logement () {
+    return <>
+        <div className="logements">
+            <p>Voici les logments proposés</p>
+        </div>
+    </>
+}
+
+export default Logement
