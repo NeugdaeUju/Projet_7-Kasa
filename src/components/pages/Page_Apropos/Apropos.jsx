dc469629d0aca79d13e06320b1542cc15603e9d1
NeugdaeUju <leila.plltr@gmail.com>
2025-09-08 12:12:35 +0200

    Création de la div qui contiendra les valeurs de l'entreprise

--- src/components/pages/Page_Apropos/Apropos.jsx
+++ src/components/pages/Page_Apropos/Apropos.jsx
@@ -1,9 +1,11 @@
 import Footer from '../../components_allPages/Footer.jsx'
 import HeroAP from './HeroAP.jsx'
+import Valeurs from './Valeurs.jsx'
 
 function Apropos () {
     return <>
     <HeroAP />
+    <Valeurs />
     <Footer />
     </>
 }
---
+++ src/components/pages/Page_Apropos/Valeurs.jsx
@@ -0,0 +1,11 @@
+import '../../../styles/style_valeurs.scss'
+
+function Valeurs () {
+    return <>
+        <div className='valeurs'>
+            <p>Test</p>
+        </div>
+    </>
+}
+
+export default Valeurs
