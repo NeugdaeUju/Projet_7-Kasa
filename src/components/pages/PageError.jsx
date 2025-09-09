6724418100d94885fcc83a5c5894d9744bf4a54d
NeugdaeUju <leila.plltr@gmail.com>
2025-09-09 14:10:30 +0200

    Ajout du header à la page

--- src/components/pages/PageError.jsx
+++ src/components/pages/PageError.jsx
@@ -1,10 +1,12 @@
 import {Link} from 'react-router-dom'
+import Header from '../components_allPages/Header.jsx'
 
 function PageError () {
     return <>
-        <h1>404</h1>
-        <p>Oups! La page que vous demandez n'existe pas.</p>
-        <Link to='/'>Retourner sur la page d'accueil</Link>
+    <Header />
+    <h1>404</h1>
+    <p>Oups! La page que vous demandez n'existe pas.</p>
+    <Link to='/'>Retourner sur la page d'accueil</Link>
     </>
 }
 
