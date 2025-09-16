23c88064ecd0f180244b1ba6d279bdb3051f7a8c
Leïla Pelletier <leila.plltr@gmail.com>
2025-09-16 14:15:09 +0200

    Modification des dropdown pour ajouter la fleche (selon la maquette) + style

--- src/components/components_allPages/Dropdown.jsx
+++ src/components/components_allPages/Dropdown.jsx
@@ -1,23 +1,33 @@
 import '../../styles/style_dropdown.css'
 import { useState } from 'react'
+import image from '../../assets/images/arrow_dropdown.png'
 
 function Dropdown ({title, content, page}) {
     const  [isOpen, setIsOpen] = useState(false);
 
-    const toggleDropdown = (e) => {
+    /*const toggleDropdown = (e) => {
         e.preventDefault();
         setIsOpen(prev => !prev);
-    }
+    }*/
 
     return <div className={`dropdown ${page === "logement" ? "dropdown--logement" : ""}`}>
-        <details open={isOpen} className='dropdown__button'>
+        <div className='dropdown__button' /*onClick={toggleDropdown}*/>
+            <p className='dropdown__button__title'>{title}</p>
+            <img src={image} className='dropdown__button__image'></img>
+        </div>
+        <div className={`dropdown__button__text ${isOpen ? "dropdown__button__text--open" : "dropdown__button__text--close"}`}>
+            <p>{content}</p>
+        </div>
+        
+    </div>
+}
+
+/* 
+<details open={isOpen} className='dropdown__button'>
             <summary className='dropdown__button__title' onClick={toggleDropdown}>{title}</summary>
             <p className={`dropdown__button__text ${isOpen ? "dropdown__button__text--open" : "dropdown__button__text--close"}`}
             >{content}</p>
         </details>
-    </div>
-}
-
-// dropdown__button__text
+*/
 
 export default Dropdown
