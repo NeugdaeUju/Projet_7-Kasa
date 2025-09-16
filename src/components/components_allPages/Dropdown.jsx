584825d2a7f7c9deda281ea99391989296dd9982
Leïla Pelletier <leila.plltr@gmail.com>
2025-09-16 14:36:28 +0200

    Correction sur les dropdown : animation sur l'ouverture et la fermeture du dropdown

--- src/components/components_allPages/Dropdown.jsx
+++ src/components/components_allPages/Dropdown.jsx
@@ -5,17 +5,14 @@ import image from '../../assets/images/arrow_dropdown.png'
 function Dropdown ({title, content, page}) {
     const  [isOpen, setIsOpen] = useState(false);
 
-    /*const toggleDropdown = (e) => {
-        e.preventDefault();
-        setIsOpen(prev => !prev);
-    }*/
+    const toggleDropdown = () => setIsOpen(prev => !prev);
 
     return <div className={`dropdown ${page === "logement" ? "dropdown--logement" : ""}`}>
-        <div className='dropdown__button' /*onClick={toggleDropdown}*/>
+        <div className={`dropdown__button ${isOpen ? "dropdown__text--open" : "dropdown__text--close"}`} onClick={toggleDropdown}>
             <p className='dropdown__button__title'>{title}</p>
             <img src={image} className='dropdown__button__image'></img>
         </div>
-        <div className={`dropdown__button__text ${isOpen ? "dropdown__button__text--open" : "dropdown__button__text--close"}`}>
+        <div className='dropdown__text'>
             <p>{content}</p>
         </div>
         
